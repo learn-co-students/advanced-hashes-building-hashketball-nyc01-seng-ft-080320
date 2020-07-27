e13c508aa5a21a3a04bccd042aac3fedfc5e88e3
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-07-27 05:14:28 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,138 @@
-# Write your code here!
+def game_hash
+  the_hash = {}
+
+  the_hash[:home] = {}
+  the_hash[:home][:team_name] = "Brooklyn Nets"
+  the_hash[:home][:colors] = ["Black", "White"]
+
+  the_hash[:home][:players] = []
+
+  the_hash[:home][:players][0] = {}
+  the_hash[:home][:players][1] = {}
+  the_hash[:home][:players][2] = {}
+  the_hash[:home][:players][3] = {}
+  the_hash[:home][:players][4] = {}
+
+  the_hash[:home][:players][0][:player_name] = "Alan Anderson"
+  the_hash[:home][:players][1][:player_name] = "Reggie Evans"
+  the_hash[:home][:players][2][:player_name] = "Brook Lopez"
+  the_hash[:home][:players][3][:player_name] = "Mason Plumlee"
+  the_hash[:home][:players][4][:player_name] = "Jason Terry"
+
+  the_hash[:home][:players][0][:number] = 0
+  the_hash[:home][:players][1][:number] = 30
+  the_hash[:home][:players][2][:number] = 11
+  the_hash[:home][:players][3][:number] = 1
+  the_hash[:home][:players][4][:number] = 31
+
+  the_hash[:home][:players][0][:shoe] = 16
+  the_hash[:home][:players][1][:shoe] = 14
+  the_hash[:home][:players][2][:shoe] = 17
+  the_hash[:home][:players][3][:shoe] = 19
+  the_hash[:home][:players][4][:shoe] = 15
+
+  the_hash[:home][:players][0][:points] = 22
+  the_hash[:home][:players][1][:points] = 12
+  the_hash[:home][:players][2][:points] = 17
+  the_hash[:home][:players][3][:points] = 26
+  the_hash[:home][:players][4][:points] = 19
+
+  the_hash[:home][:players][0][:rebounds] = 12
+  the_hash[:home][:players][1][:rebounds] = 12
+  the_hash[:home][:players][2][:rebounds] = 19
+  the_hash[:home][:players][3][:rebounds] = 11
+  the_hash[:home][:players][4][:rebounds] = 2
+
+  the_hash[:home][:players][0][:assists] = 12
+  the_hash[:home][:players][1][:assists] = 12
+  the_hash[:home][:players][2][:assists] = 10
+  the_hash[:home][:players][3][:assists] = 6
+  the_hash[:home][:players][4][:assists] = 2
+
+  the_hash[:home][:players][0][:steals] = 3
+  the_hash[:home][:players][1][:steals] = 12
+  the_hash[:home][:players][2][:steals] = 3
+  the_hash[:home][:players][3][:steals] = 3
+  the_hash[:home][:players][4][:steals] = 4
+
+  the_hash[:home][:players][0][:blocks] = 1
+  the_hash[:home][:players][1][:blocks] = 12
+  the_hash[:home][:players][2][:blocks] = 1
+  the_hash[:home][:players][3][:blocks] = 8
+  the_hash[:home][:players][4][:blocks] = 11
+
+  the_hash[:home][:players][0][:slam_dunks] = 1
+  the_hash[:home][:players][1][:slam_dunks] = 7
+  the_hash[:home][:players][2][:slam_dunks] = 15
+  the_hash[:home][:players][3][:slam_dunks] = 5
+  the_hash[:home][:players][4][:slam_dunks] = 1
+
+
+  the_hash[:away] = {}
+  the_hash[:away][:team_name] = "Charlotte Hornets"
+  the_hash[:away][:colors] = ["Turquoise", "Purple"]
+
+  the_hash[:away][:players] = []
+
+  the_hash[:away][:players][0] = {}
+  the_hash[:away][:players][1] = {}
+  the_hash[:away][:players][2] = {}
+  the_hash[:away][:players][3] = {}
+  the_hash[:away][:players][4] = {}
+
+  the_hash[:away][:players][0][:player_name] = "Jeff Adrien"
+  the_hash[:away][:players][1][:player_name] = "Bismack Biyombo"
+  the_hash[:away][:players][2][:player_name] = "DeSagna Diop"
+  the_hash[:away][:players][3][:player_name] = "Ben Gordon"
+  the_hash[:away][:players][4][:player_name] = "Kemba Walker"
+
+  the_hash[:away][:players][0][:number] = 4
+  the_hash[:away][:players][1][:number] = 0
+  the_hash[:away][:players][2][:number] = 2
+  the_hash[:away][:players][3][:number] = 8
+  the_hash[:away][:players][4][:number] = 33
+
+  the_hash[:away][:players][0][:shoe] = 18
+  the_hash[:away][:players][1][:shoe] = 16
+  the_hash[:away][:players][2][:shoe] = 14
+  the_hash[:away][:players][3][:shoe] = 15
+  the_hash[:away][:players][4][:shoe] = 15
+
+  the_hash[:away][:players][0][:points] = 10
+  the_hash[:away][:players][1][:points] = 12
+  the_hash[:away][:players][2][:points] = 24
+  the_hash[:away][:players][3][:points] = 33
+  the_hash[:away][:players][4][:points] = 6
+
+  the_hash[:away][:players][0][:rebounds] = 1
+  the_hash[:away][:players][1][:rebounds] = 4
+  the_hash[:away][:players][2][:rebounds] = 12
+  the_hash[:away][:players][3][:rebounds] = 3
+  the_hash[:away][:players][4][:rebounds] = 12
+
+  the_hash[:away][:players][0][:assists] = 1
+  the_hash[:away][:players][1][:assists] = 7
+  the_hash[:away][:players][2][:assists] = 12
+  the_hash[:away][:players][3][:assists] = 2
+  the_hash[:away][:players][4][:assists] = 12
+
+  the_hash[:away][:players][0][:steals] = 2
+  the_hash[:away][:players][1][:steals] = 22
+  the_hash[:away][:players][2][:steals] = 4
+  the_hash[:away][:players][3][:steals] = 1
+  the_hash[:away][:players][4][:steals] = 7
+
+  the_hash[:away][:players][0][:blocks] = 7
+  the_hash[:away][:players][1][:blocks] = 15
+  the_hash[:away][:players][2][:blocks] = 5
+  the_hash[:away][:players][3][:blocks] = 1
+  the_hash[:away][:players][4][:blocks] = 5
+
+  the_hash[:away][:players][0][:slam_dunks] = 2
+  the_hash[:away][:players][1][:slam_dunks] = 10
+  the_hash[:away][:players][2][:slam_dunks] = 5
+  the_hash[:away][:players][3][:slam_dunks] = 0
+  the_hash[:away][:players][4][:slam_dunks] = 12
+
+  the_hash
+end
